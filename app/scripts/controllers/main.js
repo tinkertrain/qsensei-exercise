5c77f336472611bd498b76f84489f267c399b0ed
Juan Pablo <jlomeli@itexico.net>
2014-04-28 12:14:38 -0500

    Added docs to methods on the controller, added clearAllEditing method

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,7 +4,21 @@ angular.module('qsenseiExerciseApp')
   .controller('MainCtrl', ['$scope', 'Companies',
       function($scope, Companies) {
 
-        // Get the companies data
+        /**
+         * Method to clear the edit state
+         * @param  {OBJ} companiesData The companiesData array
+         */
+        var clearAllEditing = function(companiesData) {
+          _.forEach(companiesData, function(company) {
+            if(company.editing) {
+              company.editing = false;
+            }
+          })
+        };
+
+        /**
+         * Method to fill the table with the data form the xml
+         */
         $scope.setTable = function() {
           Companies.get().then(function(data) {
             $scope.companiesData = data;
@@ -13,11 +27,21 @@ angular.module('qsenseiExerciseApp')
 
         $scope.companiesData = null;
 
+        /**
+         * Method to edit the company name
+         * @param  {OBJ} company The company to edit
+         */
         $scope.editCompany = function(company) {
+          clearAllEditing($scope.companiesData.companies.company);
           company.editing = true;
           company.focus = true;
         };
-        // Key event
+
+        /**
+         * Method to stop the edition on 'enter' keyup
+         * @param  {obj} $event  The event
+         * @param  {obj} company The company being edited
+         */
         $scope.stopEditing = function($event, company) {
           if($event.keyCode === 13 || $event.charCode === 13) {
             company.editing = false;
