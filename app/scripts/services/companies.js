2ba6a6f25589af070157f1d4108208dd53558e31
Juan Pablo <tinkertrain@gmail.com>
2014-04-27 20:19:13 -0500

    Created a service to read the companies data, received the in xml, transformed the response to json before returning it

--- app/scripts/services/companies.js
+++ app/scripts/services/companies.js
@@ -1,6 +1,25 @@
 'use strict';
 
 angular.module('qsenseiExerciseApp')
-  .service('Companies', function Companies() {
-
-  });
+  .service('Companies', ['$http', function Companies($http) {
+      return {
+        get: function() {
+          $http(
+            {
+              method: 'GET',
+              url: '/data/companies.xml',
+              transformResponse: function(xmlData, headersGetter) {
+                var x2js = new X2JS();
+                var companiesData = x2js.xml_str2json( xmlData );
+                return companiesData;
+              }
+            })
+            .success(function(companiesData) {
+              console.log(companiesData);
+            })
+            .error(function(data, status, headers, config) {
+              console.log('An error ocurred: ' + status);
+            });
+        }
+      };
+    }]);
